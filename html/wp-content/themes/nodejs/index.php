833df333f98fe0de5a3e5f936ae24eb565a30464
ju4nr3v0l <juandavid@lm.net.co>
2015-08-02 19:09:22 -0500

    html/

---
+++ html/wp-content/themes/nodejs/index.php
@@ -0,0 +1,17 @@
+<?php
+    $data = new stdclass();
+    $data->post_title = "Start";
+    $data->template = 'index'; // the template name to be read by frnt
+    $data->posts = array();
+    if (have_posts()) {
+        while (have_posts()) {
+            the_post();
+            $post_item = new stdclass();
+            $post_item->post_title = $post->post_title;
+            $post_item->permalink = get_permalink($post->ID);
+            $post_item->contenido = $post->post_content;
+            array_push($data->posts, $post_item);
+        }
+    }
+    header("content-type: application/json");
+    echo json_encode($data);
